a6ce469f5a1cd3d55b4af4b255dd7aca9d6057eb
Vu Quoc Anh <quocanh20705@gmail.com>
2025-12-18 22:14:09 +0700

    feat: Make Token Expire in 8h in stead of 30d

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,20 +1,18 @@
-// File: backend/utils/generateToken.js
-
 const jwt = require('jsonwebtoken');
 
 const generateToken = (res, userId, userRole) => {
   const token = jwt.sign(
-    { userId, role: userRole }, // Nội dung (payload) của Token
-    process.env.JWT_SECRET,    // Khóa bí mật (lấy từ .env)
-    { expiresIn: '8h' }       // Hết hạn sau 30 ngày
+    { userId, role: userRole }, 
+    process.env.JWT_SECRET,    
+    { expiresIn: '8h' }       
   );
 
-  // (Tùy chọn) Gửi Token qua HTTP-Only Cookie (Bảo mật hơn)
+
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== 'development', // Chỉ HTTPS ở production
+    secure: process.env.NODE_ENV !== 'development', 
     sameSite: 'strict',
-    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 ngày (tính bằng mili-giây)
+    maxAge: 30 * 24 * 60 * 60 * 1000 
   });
 };
 
