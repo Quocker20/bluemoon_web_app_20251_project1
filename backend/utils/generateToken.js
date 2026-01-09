ff90bfe65a9ade223aae06e5926b10ae8715d312
Vu Quoc Anh <quocanh20705@gmail.com>
2026-01-09 15:15:14 +0700

    feat: Token expires in 24h

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,7 +4,7 @@ const generateToken = (res, userId, userRole) => {
   const token = jwt.sign(
     { userId, role: userRole }, 
     process.env.JWT_SECRET,    
-    { expiresIn: '8h' }       
+    { expiresIn: '24h' }       
   );
 
 
@@ -12,7 +12,7 @@ const generateToken = (res, userId, userRole) => {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development', 
     sameSite: 'strict',
-    maxAge: 30 * 24 * 60 * 60 * 1000 
+    maxAge:  24 * 60 * 60 * 1000 
   });
 };
 
